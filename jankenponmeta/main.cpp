5d00f10d6e0fedc4eec0a980ab3621da5bec713e
janderkkotlarski <j.d.kotlarski@gmx.com>
2024-01-17 12:33:16 +0100

    Doing large rng iteration results in equal distribution for multiple equal probable outcomes

--- jankenponmeta/main.cpp
+++ jankenponmeta/main.cpp
@@ -8,11 +8,36 @@ int main(void)
 {
 
   fibran fibi;
-  const unsigned amount { 16 };
+  const unsigned amount { 10000000 };
+
+  const unsigned division { 10 };
+
+  std::vector <unsigned> hits;
+
+  for (unsigned count { 0 }; count < division; ++count) {
+    hits.push_back(0);
+  }
+
+  std::cout << division << std::endl;
+
+  for (unsigned count { 0 }; count < amount; ++count) {
+    fibi.auto_fib();
+    ++hits[fibi.get_val() % division];
+  }
+
+  for (const unsigned hit: hits) {
+    std::cout << "[" << hit << "]";
+  }
+
+  std::cout << std::endl;
+
+  std::cout << amount << std::endl;
 
   // player yugi(fibi, false);
 
   // yugi.show_sign();
+
+  /*
 
   std::vector <player> jesters;
 
@@ -39,6 +64,8 @@ int main(void)
 
   show_player_signs(jesters);
   show_player_scores(jesters);
+
+  */
 
   return 0;
 }
